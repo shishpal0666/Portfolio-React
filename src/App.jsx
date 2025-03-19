1aa4df33d5c37a12182f90956e9e616f0ecd3e32
shishpal <shishpalpolampally@gmail.com>
2025-03-19 17:10:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-blue-900 text-2xl">Shishpal Portfolio </h1>
+    </div>
+  )
+}
+
+export default App
