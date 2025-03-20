f101a774b4d979040886baecb7ee466859e5e489
shishpal <shishpalpolampally@gmail.com>
2025-03-20 23:06:47 +0530

    clean package.json

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,9 +1,10 @@
+
 const Resume = () => {
-    return (
-        <div>
-            <h1>Resume</h1>
-        </div>
-    );
+  return (
+    <div>
+      Resume
+    </div>
+  );
 };
 
 export default Resume;
